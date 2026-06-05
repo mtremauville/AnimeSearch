5f2ba042298bf068a37257fed70a47bcbe12e906
Mickael Tremauville <71016683+mtremauville@users.noreply.github.com>
2026-06-06 00:22:58 +0200

    controllers generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  get "studios/index"
+  get "studios/show"
+  get "genres/index"
+  get "genres/show"
+  get "animes/index"
+  get "animes/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
